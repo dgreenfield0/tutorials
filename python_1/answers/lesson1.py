7118ba61eca38356e835345a03db5e284a5430f8
David <dgreenfield0@users.noreply.github.com>
2020-09-23 22:20:33 -0700

    update

--- python_1/answers/lesson1.py
+++ python_1/answers/lesson1.py
@@ -6,16 +6,3 @@ try:
 finally:
     # closer the connection to the file
     reader.close()
-
-print("\n")
-
-# Read file line by line 
-with open('../lesson1_plain.txt', 'r') as reader:
-    try:
-        # Use FOR loop to interate over each line
-        for line in reader:
-            # print out the contents of the line.
-            print(line)
-    finally:
-        # Close the connection to the file
-        reader.close()
